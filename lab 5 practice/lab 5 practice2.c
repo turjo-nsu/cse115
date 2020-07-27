4b2a0df8e17b458586d1ee66af52cc4848c7a99a
turjo-nsu <68555387+turjo-nsu@users.noreply.github.com>
2020-07-27 16:37:36 +0600

    Add files via upload

---
+++ lab 5 practice/lab 5 practice2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ char choice;
+ printf("Enter a character:");
+ scanf("%c",&choice);
+ switch(choice)
+ {
+ case 'a':
+ printf("You typed a");
+ break;
+ case 'b':
+ printf("You typed b");
+ break;
+ default:
+ printf("You typed something else");
+ }
+ return 0;
+}
---
+++ lab 5 practice/lab 5 practice3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main ()
+{
+ int a,b;
+ printf("Enter the value of a and b:");
+ scanf("%d %d",&a, &b);
+ switch(a)
+ {
+ case 1:
+ printf("This is part of outer switch\n", a );
+ switch(b)
+ {
+ case 2:
+ printf("This is part of inner switch\n", a );
+ break;
+ default:
+ printf("Inner switch default value\n");
+ }
+ break;
+ default:
+ printf("Default value\n");
+ }
+ return 0;
+}
