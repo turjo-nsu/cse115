1778f02243b91b2d4439d3605b4e2c743277071d
turjo-nsu <68555387+turjo-nsu@users.noreply.github.com>
2020-09-07 21:23:20 +0600

    Add files via upload

---
+++ Week 7 lab 9 practice/1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ int var1;
+ char var2;
+ printf("Address of var1: %x\n",&var1);
+ printf("Address of var2: %x\n",&var2);
+ return 0;
+}
---
+++ Week 7 lab 9 practice/5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+ char str[]="hello";
+ char *chptr;
+ chptr=str;
+ puts(str);
+ puts(chptr);
+ return 0;
+}
